d484410ccdfde0facbaf7600664777b537336b09
Christie Lewis <christielewis@Christies-MacBook-Air.local>
2022-04-15 10:23:11 -0400

    edited auth/events.js; started coding anime/events.js file

---
+++ app/anime/events.js
@@ -0,0 +1,7 @@
+const animeApi = require('./api')
+const animeUi = require('./ui')
+const getFormFields = require('../../lib/get-form-fields')
+
+const onCreateBook = () => {
+    
+}
--- app/auth/events.js
+++ app/auth/events.js
@@ -2,18 +2,18 @@ const authApi = require('./api.js')
 const authUi = require('./ui.js')
 const getFormFields = require('../../lib/get-form-fields.js')
 
-const onWelcomeSignUp = function() {
-    // $('#welcome-pg').hide()
-    // // $('#title').hide()
-    // $('#sign-up').show()
+const onWelcomeSignUp = () => {
+    $('#welcome-pg').hide()
+    // $('#title').hide()
+    $('#sign-up').show()
 }
-const onWelcomeSignIn = function() {
-    // $('#welcome-pg').hide()
-    // $('#sign-up').hide()
-    // $('#sign-in').show()
+const onWelcomeSignIn = () => {
+    $('#welcome-pg').hide()
+    $('#sign-up').hide()
+    $('#sign-in').show()
 }
 
-const onSignUp = function(e) {
+const onSignUp = (e) => {
     e.preventDefault()
     // console.log('yerrrrr');
 
@@ -23,24 +23,23 @@ const onSignUp = function(e) {
 
     // $('#welcome-pg').show()
     // $('#welcome-pg').hide()
-    // $('#sign-up').hide()
-    // $('#sign-in').show()
-    // $('#sign-out').hide()
-    // $('#games').hide()
+    $('#sign-up').hide()
+    $('#sign-in').show()
+    $('#sign-out').hide()
     
     if(data.credentials.password !== data.credentials.password_confirmation) {
         $('#auth-status').html('<p>Passwords entered do not match.<br>Try again!</p>');
-        // $('#sign-up').show()
+        $('#sign-up').show()
     } else {
-        // $('#sign-up').hide()
-        // $('#sign-in').show()
+        $('#sign-up').hide()
+        $('#sign-in').show()
         authApi.signUp(data)
             .then(() => authUi.onSignUpSuccess())
             .catch(() => authUi.onSignUpFailure())
     }
 }
 
-const onSignIn = function(e) {
+const onSignIn = (e) => {
     e.preventDefault()
 
     // $('#sign').toggleClass('hidden')
@@ -48,23 +47,28 @@ const onSignIn = function(e) {
     const data = getFormFields(form)
     console.log(data)
 
-    // $('#welcome-pg').hide()
-    // $('#welcome-pg').hide()
-    // $('#sign-up').hide()
-    // $('#sign-in').hide()
-    // $('#sign-out').show()
-    // $('#games').show()
+    $('#welcome-pg').hide()
+    $('#sign-up').hide()
+    $('#sign-in').hide()
+    $('#sign-out').show()
+    $('#change-pw').show()
     
     authApi.signIn(data)
         .then((response) => authUi.onSignInSuccess(response))
         .catch(() => authUi.onSignInFailure())
 }
 
-const onChangePW = function(e) {
+const onChangePW = (e) => {
     e.preventDefault()
 
     const form = e.target
     const data = getFormFields(form)
+
+    $('#welcome-pg').hide()
+    $('#sign-up').hide()
+    $('#sign-in').hide()
+    $('#sign-out').show()
+    $('#change-pw').hide()
 
     authApi
         .changePW(data)
@@ -72,12 +76,13 @@ const onChangePW = function(e) {
         .catch(() => authUi.onChangePWFailure())
 }
 
-const onSignOut = function() {
+const onSignOut = () => {
 
-    // $('#sign-up').show()
-    // $('#sign-in').show()
-    // $('#sign-out').hide()
-    // $('#welcome-pg').show()
+    $('#sign-up').show()
+    $('#sign-in').show()
+    $('#sign-out').hide()
+    $('#welcome-pg').show()
+    $('#change-pw').hide()
 
     authApi
         .signOut()
